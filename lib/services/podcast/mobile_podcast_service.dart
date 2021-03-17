f7193a8e2b10cdf0d50f6687ba57e523a911acdd
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2021-03-17 12:26:32 +0300

    Resolve conflicts
    Resolve conflicts that were not fixed properly for:
    Add metadata hook.
    Update mini player theme

--- lib/services/podcast/mobile_podcast_service.dart
+++ lib/services/podcast/mobile_podcast_service.dart
@@ -272,8 +272,7 @@ class MobilePodcastService extends PodcastService {
     await repository.saveEpisode(episode);
 
     if (await hasStoragePermission()) {
-      final filepath =
-          episode.filepath == null || episode.filepath.isEmpty ? await getStorageDirectory() : episode.filepath;
+      final filepath = episode.filepath == null || episode.filepath.isEmpty ? await getStorageDirectory() : episode.filepath;
       final filename = join(filepath, episode.filename);
 
       var f = File.fromUri(Uri.file(filename));
